6f790f5370f238713a9033348e7a9e0b744b9c19
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-08-02 14:25:06 -0300

    Added more exam practice for midterm exam

--- SQL/Extra Practice/exam_practice_3.sql
+++ SQL/Extra Practice/exam_practice_3.sql
@@ -43,21 +43,121 @@ where
 
 -- Ejercicio 4
 /* Listar los clientes que han solicitado un contrato en 2024. Mostrar nombre, apellido y fecha de solicitud. */
+select
+  p.nombre,
+  p.apellido,
+  sc.fecha_solicitud
+from
+  persona p
+  inner join solicitud_contrato sc on p.id = sc.id_cliente
+where
+  year (sc.fecha_solicitud) = 2024;
+
 -- Ejercicio 5
-/* Listar las propiedades que tienen jardín. Mostrar id, dirección y zona. */
+/* Listar las propiedades que tienen jardín interno. Mostrar id, dirección y zona. */
+select
+  p.id,
+  p.direccion,
+  p.zona
+from
+  propiedad p
+  inner join caracteristica_propiedad cp on p.id = cp.id_propiedad
+  inner join caracteristica c on cp.id_caracteristica = c.id
+where
+  c.nombre = 'jardín'
+  and cp.contenido = 'si';
+
 -- Ejercicio 6
 /* Listar todas las propiedades que hayan estado alquiladas en algún momento de 2023, mostrando dirección, zona, fecha de inicio y fin del contrato, y nombre y apellido del cliente. Ordenar por fecha de inicio descendente. */
+select
+  p.direccion,
+  p.zona,
+  sc.fecha_contrato,
+  per.nombre,
+  per.apellido
+from
+  propiedad p
+  inner join solicitud_contrato sc on p.id = sc.id_propiedad
+  inner join persona per on sc.id_cliente = per.id
+where
+  sc.fecha_contrato between '2023-01-01' and '2023-12-31'
+order by
+  sc.fecha_contrato desc;
+
 -- Ejercicio 7
 /* Listar los agentes que actualmente tienen asignadas propiedades en la zona 'Centro', mostrando nombre, apellido y dirección de la propiedad. */
+select
+  p.nombre,
+  p.apellido,
+  prop.direccion
+from
+  persona p
+  inner join agente_asignado aa on p.id = aa.id_agente
+  inner join propiedad prop on aa.id_propiedad = prop.id
+where
+  aa.fecha_hora_hasta is null
+  and prop.zona = 'Centro';
+
 -- Ejercicio 8
 /* Listar los clientes que hayan solicitado contratos para propiedades en la zona 'Norte' y que el importe mensual sea mayor a $100000. Mostrar nombre, apellido, dirección de la propiedad e importe mensual. */
+select
+  per.nombre,
+  per.apellido,
+  p.direccion,
+  sc.importe_mensual
+from
+  persona per
+  inner join solicitud_contrato sc on per.id = sc.id_cliente
+  inner join propiedad p on sc.id_propiedad = p.id
+where
+  p.zona = 'Norte'
+  and sc.importe_mensual > 100000;
+
 -- Ejercicio 9
 /* Listar todas las propiedades que están disponibles para alquiler y que fueron construidas antes de 2010. Mostrar id, dirección, zona y año de construcción. Ordenar por año de construcción ascendente. */
+select
+  p.id,
+  p.direccion,
+  p.zona,
+  p.anio_const
+from
+  propiedad p
+where
+  p.situacion = 'en oferta'
+  and p.anio_const < 2010
+order by
+  p.anio_const;
+
 -- Ejercicio 10
 /* Listar las propiedades que tienen al menos una característica cuyo contenido sea 'no'. Mostrar dirección, zona y nombre de la característica. */
+select
+  p.direccion,
+  p.zona,
+  c.nombre
+from
+  propiedad p
+  inner join caracteristica_propiedad cp on p.id = cp.id_propiedad
+  inner join caracteristica c on cp.id_caracteristica = c.id
+where
+  cp.contenido = 'no';
+
 -- Ejercicio 11
-/* Listar los clientes que han solicitado contratos para propiedades en la zona 'Sur' y que el estado de la solicitud sea 'rechazada'. Mostrar nombre, apellido, dirección de la propiedad y fecha de solicitud. */
--- Resoluciones
+/* Listar los clientes que han solicitado contratos para propiedades en la zona 'Espacio Móvil' y que el estado de la solicitud sea 'rechazada'. Mostrar nombre, apellido, dirección de la propiedad y fecha de solicitud. */
+select
+  per.nombre,
+  per.apellido,
+  p.direccion,
+  sc.fecha_solicitud
+from
+  persona per
+  inner join solicitud_contrato sc on per.id = sc.id_cliente
+  inner join propiedad p on sc.id_propiedad = p.id
+where
+  p.zona = 'Espacio Móvil'
+  and sc.estado = 'rechazada';
+
+select * from propiedad;
+-- Resoluciones sugeridas
 -- Ejercicio 1
 SELECT
   id,
