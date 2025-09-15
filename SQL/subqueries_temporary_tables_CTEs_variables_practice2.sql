aeb50cf1765f17322f71f56ff12f8eea46ee512c
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-09-14 21:49:08 -0300

    Finished some exercises from the subqueries practice

--- SQL/subqueries_temporary_tables_CTEs_variables_practice2.sql
+++ SQL/subqueries_temporary_tables_CTEs_variables_practice2.sql
@@ -1,19 +1,135 @@
--- use agencia_personal;
--- Exercise 7
-/* Mostrar los empleados cuyo salario supere al promedio de sueldo de la empresa que los contrató.  */
-drop table if exists emp_prom;
+use agencia_personal;
 
-create TEMPORARY table emp_prom (
-  select
-    cuit,
-    avg(sueldo)
-  from
-    contratos
-  group by
-    cuit
-);
+/*
+ * Exercise 1
+ * ¿ Qué personas fueron contratadas por las mismas empresas que Stefanía Lopez ? 
+ */
+select
+  distinct p.dni,
+  p.apellido,
+  p.nombre
+from
+  personas p
+  inner join contratos c on p.dni = c.dni
+where
+  c.cuit in (
+    select
+      c2.cuit
+    from
+      contratos c2
+      inner join personas p2 on c2.dni = p2.dni
+    where
+      p2.nombre = 'Stefanía'
+      and p2.apellido = 'Lopez'
+  );
+
+/* 
+ * Exercise 2 
+ * Encontrar a aquellos empleados que ganan menos que el máximo sueldo de los empleados de Viejos Amigos.
+ */
+select
+  max(con.sueldo) into @max_sueldo
+from
+  contratos con
+  inner join empresas emp on emp.cuit = con.cuit
+where
+  emp.razon_social = 'Viejos Amigos';
 
 select
-  *
+  p.dni,
+  concat(p.nombre, ' ', p.apellido),
+  c.sueldo
 from
-  emp_prom;
+  personas p
+  inner join contratos c on p.dni = c.dni
+where
+  c.sueldo < @max_sueldo;
+
+/*
+ * Exercise 3
+ * Mostrar empresas contratantes y sus promedios de comisiones pagadas o a pagar, 
+ * pero sólo de aquellas cuyo promedio supere al promedio de Tráigame eso. 
+ */
+select
+  avg(com.importe_comision) into @prom_traigame_eso
+from
+  contratos con
+  inner join empresas emp on emp.cuit = con.cuit
+  inner join comisiones com on com.nro_contrato = con.nro_contrato
+where
+  emp.razon_social = 'Traigame eso'
+group by
+  con.cuit;
+
+select
+  emp.cuit,
+  emp.razon_social,
+  avg(com.importe_comision) prom
+from
+  empresas emp
+  inner join contratos con on emp.cuit = con.cuit
+  inner join comisiones com on con.nro_contrato = com.nro_contrato
+where
+  emp.razon_social <> 'Tráigame eso'
+group by
+  emp.cuit,
+  emp.razon_social
+having
+  prom > @prom_traigame_eso;
+
+-- O con una subconsulta
+select
+emp.cuit,
+emp.razon_social,
+avg(com.importe_comision) prom
+from
+  empresas emp
+  inner join contratos con on emp.cuit = con.cuit
+  inner join comisiones com on con.nro_contrato = com.nro_contrato
+where
+  emp.razon_social <> 'Tráigame eso'
+group by
+  emp.cuit,
+  emp.razon_social
+having
+  prom > (
+    select
+      avg(com.importe_comision)
+    from
+contratos con
+inner join empresas emp on emp.cuit = con.cuit
+inner join comisiones com on com.nro_contrato = con.nro_contrato
+where
+  emp.razon_social = 'Traigame eso'
+group by
+con.cuit
+);
+
+/*
+ * Exercise 4
+ * Seleccionar las comisiones pagadas que tengan un importe menor al promedio 
+ * de todas las comisiones(pagas y no pagas), mostrando razón social de la 
+ * empresa contratante, mes contrato, año contrato , nro. contrato, 
+ * nombre y apellido del empleado. 
+ */
+select
+  emp.razon_social,
+  per.nombre,
+  per.apellido,
+  con.nro_contrato,
+  com.mes_contrato,
+  com.anio_contrato,
+  com.importe_comision
+from
+  comisiones com
+  inner join contratos con on con.nro_contrato = com.nro_contrato
+  inner join empresas emp on emp.cuit = con.cuit
+  inner join personas per on per.dni = con.dni
+where
+  com.fecha_pago is not null
+  and com.importe_comision < (
+select
+avg(importe_comision)
+from
+comisiones
+);
