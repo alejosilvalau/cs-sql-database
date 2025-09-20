89e5f0bec1ab23701c817adaa0fa6dac8c25e4a5
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-09-20 17:10:22 -0300

    Added more extra practice for the third SQL exam

--- SQL/Extra Practice/Third Exam/exam_practice.sql
+++ SQL/Extra Practice/Third Exam/exam_practice.sql
@@ -63,3 +63,68 @@ where
   cantidad_sector > @promedio;
 
 drop temporary table if exists cant_total_sector;
+/* 301.4 - Actividades recientes para realizar.
+ * Listar las actividades de las que se vayan a realizar cursos 
+ * a futuro pero no se haya realizado ninguno antes de este año.
+ * Indicar número y nombre de la actividad.Ordenar por número 
+ * de actividad ascendente.*/
+-- Se consideró el año actual como 2024 para tener data de la base de datos
+select
+  distinct a.numero,
+  a.nombre
+from
+  curso c
+  inner join actividad a on c.numero_actividad = a.numero
+where
+  c.fecha_inicio >= '2024-01-01'
+  and a.numero not in (
+    select
+      c_sub.numero_actividad
+    from
+      curso c_sub
+    where
+      c_sub.fecha_inicio < '2024-01-01'
+      and c_sub.numero_actividad = a.numero
+  )
+order by
+  a.numero;
+
+/* 301.5 - Socios con más cursos.
+ * Listar los socios que se inscribieron a más cursos en 2024 que el promedio 
+ * de la cantidad de cursos a la que se inscribió cada socio durante el 2024.
+ * Se debe considerar los socios que no se inscribieron a ningún curso en 2024 
+ * dentro del cálculo del promedio.Indicar número, nombre del socio y 
+ * cantidad de cursos que realiza en 2024.
+ * Ordenar por cantidad de cursos descendente. */
+drop temporary table if exists cant_ins_socios;
+
+create temporary table if not exists cant_ins_socios
+select
+  s.numero,
+  s.nombre,
+  count(s.numero) cant_ins
+from
+  socio s
+  left join inscripcion i on s.numero = i.numero_socio
+where
+  year(i.fecha_hora_inscripcion) = 2024
+group by
+  s.numero;
+
+select
+  avg(cant_ins) into @prom_cant_ins
+from
+  cant_ins_socios;
+
+select
+  numero,
+  nombre,
+  cant_ins
+from
+  cant_ins_socios
+where
+  cant_ins > @prom_cant_ins
+order by
+  cant_ins desc;
+
+drop temporary table if exists cant_ins_socios;
