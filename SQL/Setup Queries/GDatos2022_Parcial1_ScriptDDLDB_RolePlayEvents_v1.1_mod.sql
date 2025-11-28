51f9035b2b7d060932ce721a30905a86493616ed
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-11-28 08:48:12 -0300

    New schemas and ERR diagrams for doing exercises

--- SQL/Setup Queries/GDatos2022_Parcial1_ScriptDDLDB_RolePlayEvents_v1.1_mod.sql
+++ SQL/Setup Queries/GDatos2022_Parcial1_ScriptDDLDB_RolePlayEvents_v1.1_mod.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `role_play_events_mod_mod` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `role_play_events_mod_mod`;
+CREATE DATABASE  IF NOT EXISTS `role_play_events_mod` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `role_play_events_mod`;
 -- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
 --
 -- Host: 127.0.0.1    Database: role_play_events_mod
